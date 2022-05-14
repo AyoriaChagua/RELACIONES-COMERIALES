5f1547e7642a31aa75148c46bb5ed50b58ced6f0
AyoriaChagua <85888381+AyoriaChagua@users.noreply.github.com>
2022-05-14 14:31:31 -0500

    Add files via upload

---
+++ views/paginaRel.php
@@ -0,0 +1,228 @@
+<?php include('../includes/header.php')?>
+<?php include('../controllers/verify.php')?>
+<!--CABE RESALTAR QUE ESTA NO ES UNA PAGINA FUNCIONAL, ES UNA VISTA DE LAS RELACIONES QUE TIENE EL PAIS ACTUALMENTE EN REALIDAD-->
+</div>
+        <!--End of Hedaer Section-->
+   <div class="swiper-container">
+    <div class="swiper-wrapper">
+
+      <div class="swiper-slide" style="background-image: url(../imagenes/importar.jpg);"></div>
+      <div class="swiper-slide" style="background-image: url(../imagenes/avion.jpg);"></div>     
+    </div>
+    <!-- Add Arrows -->
+    <div class="swiper-button-next"></div>
+    <div class="swiper-button-prev"></div>
+  </div>
+               
+		
+                           
+                       
+ <section id="contenedor">
+
+      <div style="background-color: #ffffff" class="container">
+	    <div class="row">
+      <div class="col-md-3  cont_lat">
+      <div class="TiT_Prin_Lateral">
+      <hr class="new1">
+      ¿Qué Acuerdos Comerciales
+&nbsp;tenemos?
+        <hr class="new1">
+        <table width="100%" class="center" border="2">
+  <tr>
+    <th colspan="4"><span class="SUb_TiT_Prin_Lateral"></span></th>
+  </tr>
+  <tr>
+    <td class="style1"><a href="#"><img src="../imagenes/flag/ban_omc.png" alt="Comunidad Andina"></a></td>
+     <td class="style1"><a href="#"><img src="../imagenes/flag/ban_can.png" alt="Comunidad Andina"></a></td>
+     <td class="style1"><a href="#"><img src="../imagenes/flag/ban_mercosur.png" alt="Mercosur"></a></td>
+     <td class="style1"><a href="#"><img src="../imagenes/flag/ban_cuba.png" alt="Cuba"></a></td>
+  </tr>
+    <tr>
+    <td class="style1"><a href="#"><img src="../imagenes/flag/ban_apec.png" alt="Foro de Cooperación Económica Asia Pacífico"></a></td>
+     <td class="style1"><a href="#"><img src="../imagenes/flag/ban_chile.png" alt="Chile"></a></td>
+     <td class="style1"><a href="#"><img src="../imagenes/flag/ban_mexico.png" alt="México"></a></td>
+     <td class="style1"><a href="#"><img src="../imagenes/flag/ban_eeuu.png" alt="Estados Unidos"></a></td>
+  </tr>
+    <tr>
+    <td class="style2"><a href="#"><img src="../imagenes/flag/ban_canada.png" alt="Canadá"></a></td>
+     <td class="style2"><a href="#"><img src="../imagenes/flag/ban_singapur.png" alt="Singapur"></a></td>
+     <td class="style2"><a href="#"><img src="../imagenes/flag/ban_china.png" alt="China"></a></td>
+     <td class="style2"><a href="#"><img src="../imagenes/flag/ban_efta.png" alt="Efta"></a></td>
+  </tr>
+   <tr>
+    <td class="style2"><a href="#"><img src="../imagenes/flag/ban_korea_sur.png" alt="Corea del Sur"></a></td>
+     <td class="style2"><a href="#"><img src="../imagenes/flag/ban_tailandia.png" alt="Tailandia"></a></td>
+     <td class="style2"><a href="#"><img src="../imagenes/flag/ban_japon.png" alt="Japón"></a></td>
+     <td class="style2"><a href="#"><img src="../imagenes/flag/ban_panama.png" alt="Panamá"></a></td>
+  </tr>
+   <tr>
+    <td class="style2"><a href="#"><img src="../imagenes/flag/ban_union_europea.png" alt="Unión Europea"></a></td>
+     <td class="style2"><a href="#"><img src="../imagenes/flag/ban_costa_rica.png" alt="Costa Rica"></a></td>
+     <td class="style2"><a href="#"><img src="../imagenes/flag/ban_venezuela.png" alt="Venezuela"></a></td>
+     <td class="style2"><a href="#"><img src="../imagenes/flag/ban_ap.png" alt="Alianza del Pacífico"></a></td>
+  </tr>
+   <tr>
+    <td class="style2"><a href="#"><img src="../imagenes/flag/ban_honduras.png" alt="Honduras"></a></td>
+     <td class="style2"><a href="#"><img src="../imagenes/flag/australia.png" alt="Australia"></a></td>
+        <td class="style2"><a href="#"><img src="../imagenes/flag/bandera_reino_unido.jpg" alt="Reino Unido"></a></td>
+	    <td class="style1"><a href="#"><img src="../imagenes/flag/bandera_cptpp.png" alt="CPTPP"></a></td>
+  </tr>
+   
+</table>
+
+<table width="100%" class="center" border="2">
+  <tr>
+    <th colspan="5"><span class="SUb_TiT_Prin_Lateral2"></span></th>
+  </tr>
+  <tr>
+    <td width="25%" class="style1"><a href="#"><img src="../imagenes/flag/ban_guatemala.png" alt="Guatemala"></a></td>
+     <td width="25%"  class="style1"><a href="#"><img src="../imagenes/flag/ban_tpp.png" alt="Acuerdo de Asociación Transpacífico - TPP"></a></td>
+     <td width="25%"  class="style1"><a href="#"><img src="../imagenes/flag/bandera_brasil.png" alt="Brasil"></a></td>
+     <td width="25%"  class="style1">&nbsp;</td>
+  </tr>
+    
+   
+</table>
+
+<table width="100%" class="center" border="2">
+  <tr>
+    <th colspan="4"><span class="SUb_TiT_Prin_Lateral3"></span></th>
+  </tr>
+  <tr>
+    <td class="style1"><a href="#"><img src="../imagenes/flag/ban_doha.png" alt="Programa DOHA para el desarrollo"></a></td>
+     <td class="style1"><a href="#"><img src="../imagenes/flag/ban_tisa.png" alt="Tisa"></a></td>
+     <td class="style1"><a href="#"><img src="../imagenes/flag/ban_el_salvador.png" alt="El Salvador"></a></td>
+     <td class="style1"><a href="#"><img src="../imagenes/flag/ban_turquia.png" alt="Turquía"></a></td>
+  </tr>
+    <tr>
+    <td class="style1"><a href="#"><img src="../imagenes/flag/bandera_india.png" alt="India"></a></td>
+     <td class="style1"><a href="#"><img src="../imagenes/flag/ban_ap.png" alt="Alianza del Pacífico"></a></td>
+     <td class="style1"><a href="#"><img src="../imagenes/flag/ban_nicaragua.png" alt="Nicaragua"></a></td>
+  </tr>
+   
+</table>
+        </div></div>
+        <div class="col-md-7"><span class="TiT_Azul" method="post" name="uno" >CPTPP: Tratado Integral y Progresista de Asociación Transpacífico </span>
+        <br />
+        <br />
+
+
+
+          <span class="Text_contenido">El CPTPP entró en vigencia para el Perú el 19 de septiembre de 2021. Está conformado por 11 economías del Asia Pacífico: Australia, Brunéi, Canadá, Chile, Japón, Malasia, México, Nueva Zelanda, Singapur, Vietnam y Perú. En conjunto, estás representan el 15% del comercio internacional, el 13% del PBI mundial y un mercado de 500 millones de consumidores.  
+          <div id="form">
+          <br /> 
+  <br /><img src="../imagenes/CPTPP/nuevo.jpg" alt="Logotipo Ministerio de Comercio Exterior y Turismo">
+</span>
+
+<hr class="new1">
+			<br />
+        <br />
+			
+			<span class="TiT_Azul">Acuerdo Comercial entre el Perú y el Reino Unido</span>
+        <br />
+        <br />
+<span class="Text_contenido">El acuerdo entró en vigor el 31 de diciembre de 2020 a las 18:00 horas. Este acuerdo incorpora por referencia el Acuerdo Comercial con la Unión Europea, efectuándose únicamente modificaciones para asegurar la operatividad del nuevo Acuerdo y la continuidad de las preferencias que en la actualidad aplican al relacionamiento comercial entre Colombia, Ecuador y el Perú con el Reino Unido. 
+  <br /> <br /><img src="../imagenes/Reino_Unido/jhon.jpg" alt="Logotipo Ministerio de Comercio Exterior y Turismo">
+</span>
+<hr class="new1">
+			
+			
+			
+			
+          
+          </br>
+          <br /><img src="../imagenes/Australia/Acuerdo_de_Libre_Comercio_Per_-_Australia.png" alt="Logotipo Ministerio de Comercio Exterior y Turismo">
+           
+           </br> </br>
+           <span class="TiT_Rojo">Acuerdo de Libre Comercio Perú – Australia</span><br /><br />
+           <span class="Text_contenido">El ALC Perú – Australia entró en vigencia el 11 de febrero de 2020 y es uno de los acuerdos comerciales bilaterales más ambiciosos que el Perú ha suscrito. Cabe señalar que, el ALC incluye un capítulo sobre Pequeñas y Medianas Empresas (PYMEs), Desarrollo, y Competitividad y Facilitación de Negocios.
+  <br /> <br /><img src="../imagenes/Australia/republia.jpg" alt="Logotipo Ministerio de Comercio Exterior y Turismo">
+</span>
+<hr class="new1"><br /> <br />
+ <span class="TiT_Azul">Convenio de Cooperación Aduanera Peruano - Colombiano</span><br /><br />
+ <span class="Text_contenido">Intercambio de Notas entre la República del Perú y la República de Colombia para el retiro de subpartidas arancelarias del arancel común establecido en el Anexo del Protocolo Modificatorio del Convenio de Cooperación Aduanera Peruano - Colombiano de 1938 ”.
+ <br /><br />
+ Descargar documento <br /> <br />
+ <hr class="new1">
+          
+          </br>
+          <br /><img src="../imagenes/afc_img.jpg" alt="Logotipo Ministerio de Comercio Exterior y Turismo">
+           
+           </br> </br>
+           <span class="TiT_Azul">
+El Acuerdo sobre Facilitación del Comercio de la OMC</span><br /><br />
+           <span class="Text_contenido">El 22 de febrero de 2017 entró en vigencia el Acuerdo sobre Facilitación del Comercio (AFC) de la Organización Mundial del Comercio (OMC). Su importancia radica en que establece medidas enfocadas en brindar transparencia a las normas vinculadas al comercio exterior, agilizar y simplificar los trámites relacionados al despacho de las mercancías, reducir las trabas al flujo de mercancías y a generar equidad en las decisiones de las entidades de control.
+  <br /> <br />
+</span>
+<br />
+ • AFC Acuerdo sobre Facilitación del Comercio de la OMC (español) (inglés) <br /> <br />
+          <div> 
+				<div class="section-header"></div> </div>
+                <!--End of row-->
+         
+            <!--End of container-->
+        </section>
+        <!--End of purches-->
+
+ </section>
+ 
+
+        <!--End of footer-->
+
+        <!--Scroll to top-->
+        <a href="#" id="back-to-top" title="Back to top">&uarr;</a>
+        <!--End of Scroll to top-->
+
+
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <!-- <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.2/jquery.min.js'></script>-->
+        <script src="js/jquery-1.12.3.min.js"></script>
+
+        <!--Counter UP Waypoint-->
+        <script src="js/waypoints.min.js"></script>
+        <!--Counter UP-->
+        <script src="js/jquery.counterup.min.js"></script>
+
+       
+
+      
+
+        <!--Back To Top-->
+		<script src="https://kit.fontawesome.com/yourcode.js"></script>
+   
+
+
+        <!--JQuery Click to Scroll down with Menu-->
+        <script src="js/jquery.localScroll.min.js"></script>
+        <script src="js/jquery.scrollTo.min.js"></script>
+        <!--WOW With Animation-->
+        <script src="js/wow.min.js"></script>
+		<script src="js/swiper.min.js"></script>
+        <!--WOW Activated-->
+       
+	 <script>
+    var swiper = new Swiper('.swiper-container', {
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
+  </script>
+
+        <!-- Include all compiled plugins (below), or include individual files as needed -->
+        <script src="js/bootstrap.min.js"></script>
+        <!-- Custom JavaScript-->
+        <script src="js/main.js"></script>
+<script>
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+</script>
+
+<?php include('../includes/footer.php')?>
+
