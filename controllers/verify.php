2457417af56dfb6f8999f0b5b06a70dfd4e0231b
AyoriaChagua <85888381+AyoriaChagua@users.noreply.github.com>
2022-05-14 14:47:50 -0500

    Update verify.php

--- controllers/verify.php
+++ controllers/verify.php
@@ -1,6 +1,7 @@
 <?php
     session_start();
     require 'db.php';
+    //La verificación se realizará por medio del email, si este existe se crearán las variables con las que interactuaremos
     if(isset($_SESSION['email']) && !empty($_SESSION['email'])){
         $email = $_SESSION['email'];
         $get_datos_usuario = $conn->query("SELECT * FROM users WHERE email = '$email'");
@@ -8,7 +9,7 @@
         $name = $dato_usuario["name"];
         $type = $dato_usuario["type"]; 
         $email = $dato_usuario["email"];
-    }else{
+    }else{//Si no se a validado se enviará a la página de inicio
         header('Location: ../index.php');
         exit;
     }
