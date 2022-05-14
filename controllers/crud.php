a983974362b287e76f1b8514c3d05b6937f8638f
AyoriaChagua <85888381+AyoriaChagua@users.noreply.github.com>
2022-05-14 14:35:01 -0500

    Update crud.php

--- controllers/crud.php
+++ controllers/crud.php
@@ -4,15 +4,14 @@
 $queryExportar= $conn->query("SELECT * FROM exportados order by id_exportado");
 $queryImportar=$conn->query("SELECT * FROM importados order by id_importado");
  
- /////////// INSERTAR REGISTRO A LA TABLA 1 ///////////////////////
 if(isset($_POST['insertar']))// SI SE PRESIONA EL BOTÓN INSERTAR OCURRE LO SIGUIENTE:
 {
     $pais = $conn->real_escape_string($_POST['pais']);
     $producto = $conn->real_escape_string($_POST['producto']);
-    if(isset($_POST['exportar'])){
+    if(isset($_POST['exportar'])){//SI SE SELECCIONÓ LA OPCIÓN DE EXPORTAR SE INSERTARÁ EN LA TABLA DE PRODS EXPORTADOS
         $SQL = $conn->query("INSERT INTO exportados( pais_exportado, producto_exportado)
             VALUES('$pais','$producto')");
-    }else if (isset($_POST['importar'])){
+    }else if (isset($_POST['importar'])){//SI NO, SE INSERTARÁ EN LA  TABLA DE PRODS IMPORTADOS
         $SQL = $conn->query("INSERT INTO importados( pais_importado, producto_importado)
             VALUES('$pais','$producto')");
     }
